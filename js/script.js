b19c922acdf19e6e9604eb0c58a08e4b1f95e659
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-07-30 17:47:38 +0900

    add pagetop

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 $(function() {
 
-	$('#back a').on('click',function(){
-		$('body, html').animate({
-			scrollTop:0
-		},800);
-		return false;
-	});
+  //back内の<a>タグがクリックされたときの処理
+	$('#back a').on('click',function(event){
 
+		//animate()はアニメーション効果を設定するjQueryの関数
+    $('body, html').animate({
+
+      //変化対象のプロパティ:変化値
+			scrollTop:0
+
+      //動作時間
+      //ミリ秒指定以外にもslow normal fastで指定も可能
+		},800);
+
+    //<a>タグを無効にするメソッド
+		event.preventDefault();
+	});
 });
